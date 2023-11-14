2047d75eebb554280245e9c1dd75ceae40e96e1d
ImamBiplob <imamhbiplob@gmail.com>
2023-11-15 01:53:09 +0600

    add solution for Majority Element using Moore Voting Algorithm

--- Top Interview 150/MajorityElement.java
+++ Top Interview 150/MajorityElement.java
@@ -47,4 +47,23 @@ public class MajorityElement {
 
         return majorityElement;
     }
+
+    public int majorityElementUsingMooreVotingAlg(int[] nums) {
+        int count = 0;
+        int candidate = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+
+            if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        return candidate;
+    }
 }
