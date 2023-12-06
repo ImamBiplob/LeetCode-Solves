e13e4342a50a21be24ffb7c6e95f39addabad61b
ImamBiplob <imamhbiplob@gmail.com>
2023-12-07 01:14:16 +0600

    add Valid Parentheses

---
+++ Top Interview 150/ValidParentheses.java
@@ -0,0 +1,42 @@
+import java.util.Stack;
+
+public class ValidParentheses {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(')
+                stack.push(')');
+            else if (c == '{')
+                stack.push('}');
+            else if (c == '[')
+                stack.push(']');
+            else if (stack.isEmpty() || stack.pop() != c)
+                return false;
+        }
+
+        return stack.isEmpty();
+    }
+
+    public boolean isValid2(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.peek();
+                if ((c == ')' && top == '(') || (c == ']' && top == '[') || (c == '}' && top == '{')) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
