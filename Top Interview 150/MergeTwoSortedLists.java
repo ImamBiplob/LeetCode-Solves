edf4c49e36bac7bf924c919274acc9e2351e0ba2
ImamBiplob <imamhbiplob@gmail.com>
2023-12-11 23:28:48 +0600

    add Merge Two Sorted Lists

---
+++ Top Interview 150/MergeTwoSortedLists.java
@@ -0,0 +1,21 @@
+public class MergeTwoSortedLists {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode resultList = new ListNode();
+        ListNode currentNode = resultList;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                currentNode.next = list1;
+                list1 = list1.next;
+            } else {
+                currentNode.next = list2;
+                list2 = list2.next;
+            }
+            currentNode = currentNode.next;
+        }
+
+        currentNode.next = list1 == null ? list2 : list1;
+
+        return resultList.next;
+    }
+}
