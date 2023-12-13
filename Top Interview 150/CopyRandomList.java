e912f0d0bffeed11cd2604c8230cdaa303daebd7
ImamBiplob <imamhbiplob@gmail.com>
2023-12-14 01:13:27 +0600

    add Copy List with Random Pointer

---
+++ Top Interview 150/CopyRandomList.java
@@ -0,0 +1,36 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class CopyRandomList {
+    Map<Node, Node> map = new HashMap<>();
+
+    public Node copyRandomList(Node head) {
+        if (head == null) {
+            return null;
+        }
+
+        if (map.containsKey(head)) {
+            return map.get(head);
+        }
+
+        Node temp = new Node(head.val);
+        map.put(head, temp);
+
+        temp.next = copyRandomList(head.next);
+        temp.random = copyRandomList(head.random);
+
+        return temp;
+    }
+}
+
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
