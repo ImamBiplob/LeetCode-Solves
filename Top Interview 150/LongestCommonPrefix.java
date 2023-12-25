277021ad409e1d50bb87da8977055c9963df8fbe
ImamBiplob <imamhbiplob@gmail.com>
2023-12-26 01:54:54 +0600

    add Longest Common Prefix Better Solution

--- Top Interview 150/LongestCommonPrefix.java
+++ Top Interview 150/LongestCommonPrefix.java
@@ -3,18 +3,28 @@ import java.util.Arrays;
 public class LongestCommonPrefix {
     public String longestCommonPrefix(String[] strs) {
         Arrays.sort(strs);
-        String firsString = strs[0];
+        String firstString = strs[0];
         String lastString = strs[strs.length - 1];
         int index = 0;
 
-        while (index < firsString.length() && index < lastString.length()) {
-            if (firsString.charAt(index) == lastString.charAt(index)) {
+        while (index < firstString.length() && index < lastString.length()) {
+            if (firstString.charAt(index) == lastString.charAt(index)) {
                 index++;
             } else {
                 break;
             }
         }
 
-        return firsString.substring(0, index);
+        return firstString.substring(0, index);
+    }
+
+    public String longestCommonPrefixBetterSolution(String[] strs) {
+        String prefix = strs[0];
+
+        for (String s : strs)
+            while (!s.startsWith(prefix))
+                prefix = prefix.substring(0, prefix.length() - 1);
+
+        return prefix;
     }
 }
