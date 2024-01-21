4bd53fdd4a43fe4e479fc8aa924d2f6ae6f99398
ImamBiplob <imamhbiplob@gmail.com>
2024-01-22 01:52:40 +0600

    add House Robber

---
+++ Daily Challenges/HouseRobber.java
@@ -0,0 +1,14 @@
+public class HouseRobber {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        int pre = 0, cur = 0;
+
+        for (int i = 0; i < n; i++) {
+            int temp = Math.max(pre + nums[i], cur);
+            pre = cur;
+            cur = temp;
+        }
+
+        return cur;
+    }
+}
