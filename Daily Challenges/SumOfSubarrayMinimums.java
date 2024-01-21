96e831077a9fbc42325d730d4cde3cebb80882de
ImamBiplob <imamhbiplob@gmail.com>
2024-01-21 23:28:05 +0600

    add other solutions for Sum of Subarray Minimums

--- Daily Challenges/SumOfSubarrayMinimums.java
+++ Daily Challenges/SumOfSubarrayMinimums.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 import java.util.Deque;
 import java.util.ArrayDeque;
+import java.util.Stack;
 
 public class SumOfSubarrayMinimums {
     public int sumSubarrayMins(int[] arr) {
@@ -44,5 +45,60 @@ public class SumOfSubarrayMinimums {
         }
 
         return (int) answer;
+    }
+
+    public static final int MOD = 1_000_000_007;
+
+    public int sumSubarrayMins2(int[] arr) {
+        long sumOfMin = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            sumOfMin = (sumOfMin + findSubArrayMin(arr, i)) % MOD;
+        }
+
+        return (int) sumOfMin;
+    }
+
+    public static int findSubArrayMin(int[] arr, int start) {
+        int minVal = arr[start];
+        int result = 0;
+
+        for (int i = start; i < arr.length; i++) {
+            minVal = Math.min(minVal, arr[i]);
+            result += minVal;
+        }
+
+        return result;
+    }
+
+    public int sumSubarrayMins3(int[] arr) {
+        int n = arr.length;
+        Stack<Integer> s1 = new Stack<>();
+        Stack<Integer> s2 = new Stack<>();
+        long[] left = new long[n];
+        long[] right = new long[n];
+
+        for (int i = 0; i < n; ++i) {
+            while (!s1.isEmpty() && arr[s1.peek()] > arr[i]) {
+                s1.pop();
+            }
+            left[i] = s1.isEmpty() ? i + 1 : i - s1.peek();
+            s1.push(i);
+        }
+
+        for (int i = n - 1; i >= 0; --i) {
+            while (!s2.isEmpty() && arr[s2.peek()] >= arr[i]) {
+                s2.pop();
+            }
+            right[i] = s2.isEmpty() ? n - i : s2.peek() - i;
+            s2.push(i);
+        }
+
+        long result = 0;
+        for (int i = 0; i < n; ++i) {
+            result = (result + arr[i] * left[i] * right[i]) % MOD;
+        }
+
+        return (int) result;
     }
 }
