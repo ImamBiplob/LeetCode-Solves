a6e6aca46ccb998a4de1e5a032468629e2e6f5bd
ImamBiplob <imamhbiplob@gmail.com>
2024-01-28 15:15:44 +0600

    add better solution for Out of Boundary Paths

--- Daily Challenges/OutOfBoundaryPaths.java
+++ Daily Challenges/OutOfBoundaryPaths.java
@@ -27,4 +27,35 @@ public class OutOfBundaryPaths {
 
         return count;
     }
+
+    public Integer[][][] dp;
+    public int mod = 1_000_000_000 + 7;
+    public int m, n;
+
+    public int findPaths2(int m, int n, int maxMove, int x, int y) {
+        dp = new Integer[m][n][maxMove + 1];
+        this.m = m;
+        this.n = n;
+        return helper(maxMove, x, y);
+    }
+
+    private int helper(int maxMove, int x, int y) {
+        if (x < 0 || x >= m || y < 0 || y >= n)
+            return 1;
+        if (maxMove <= 0)
+            return 0;
+        if (dp[x][y][maxMove] != null) {
+            return dp[x][y][maxMove];
+        }
+        int res = 0;
+
+        res = (res + helper(maxMove - 1, x + 1, y)) % mod;
+        res = (res + helper(maxMove - 1, x, y - 1)) % mod;
+        res = (res + helper(maxMove - 1, x - 1, y)) % mod;
+        res = (res + helper(maxMove - 1, x, y + 1)) % mod;
+
+        dp[x][y][maxMove] = res;
+
+        return res;
+    }
 }
