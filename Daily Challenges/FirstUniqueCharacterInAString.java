374f9716eddbe3d5ae5be8b0a37c9beda6cea80e
ImamBiplob <imamhbiplob@gmail.com>
2024-02-05 23:59:20 +0600

    add First Unique Character in a String

---
+++ Daily Challenges/FirstUniqueCharacterInAString.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class FirstUniqueCharacterInAString {
+    public int firstUniqChar(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (char ch : s.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (map.get(s.charAt(i)) == 1)
+                return i;
+        }
+
+        return -1;
+    }
+
+    public int firstUniqChar2(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (char ch : s.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        int idx = 0;
+        for (char ch : s.toCharArray()) {
+            if (map.get(ch) == 1)
+                return idx;
+            idx++;
+        }
+
+        return -1;
+    }
+}
