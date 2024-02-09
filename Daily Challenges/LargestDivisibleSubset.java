adfe8cf1840bb5066ca9c31ac8b4af66f6eb335f
ImamBiplob <imamhbiplob@gmail.com>
2024-02-10 01:55:54 +0600

    add Largest Divisible Subset

---
+++ Daily Challenges/LargestDivisibleSubset.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class LargestDivisibleSubset {
+    List<Integer> res;
+    int[] mem;
+
+    public List<Integer> largestDivisibleSubset(int[] nums) {
+        Arrays.sort(nums);
+        res = new ArrayList<>();
+
+        mem = new int[nums.length];
+        Arrays.fill(mem, -1);
+        helper(nums, 0, new ArrayList<>(), 1);
+
+        return res;
+    }
+
+    private void helper(int[] nums, int index, List<Integer> curr, int prev) {
+        if (curr.size() > res.size()) {
+            res = new ArrayList<>(curr);
+        }
+
+        for (int i = index; i < nums.length; i++) {
+            if (curr.size() > mem[i] && nums[i] % prev == 0) {
+                mem[i] = curr.size();
+                curr.add(nums[i]);
+                helper(nums, i + 1, curr, nums[i]);
+                curr.remove(curr.size() - 1);
+            }
+        }
+    }
+}
