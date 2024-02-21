187050db884a0021233e438682a43d73b4609ab9
ImamBiplob <imamhbiplob@gmail.com>
2024-02-22 00:15:04 +0600

    add Bitwise AND of Numbers Range

---
+++ Daily Challenges/BitwiseANDOfNumbersRange.java
@@ -0,0 +1,12 @@
+public class BitwiseANDOfNumbersRange {
+    public int rangeBitwiseAnd(int left, int right) {
+        int cnt = 0;
+        while (left != right) {
+            left >>= 1;
+            right >>= 1;
+            cnt++;
+        }
+
+        return (left << cnt);
+    }
+}
